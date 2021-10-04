2c1cb145098a84eec2a5dfc8442caad7348c0c79
James Walker <walkah@walkah.net>
2021-10-04 14:23:34 -0400

    pin nix versions (with niv)

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell {
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
   name = "publish-action";
   buildInputs = with pkgs; [ act bashInteractive nodejs-12_x ];
 }
